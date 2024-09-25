29e5d1e1158e79aba4ebebceabd39a891a9611f7
AnderNoleon <andersonomar428@gmail.com>
2024-09-24 22:05:04 -0600

    cambios parte 2

--- app/src/main/java/com/example/wavesoffood/SignActivity.kt
+++ app/src/main/java/com/example/wavesoffood/SignActivity.kt
@@ -6,24 +6,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.wavesoffood.databinding.ActivityLoginBinding
+import com.example.wavesoffood.databinding.ActivitySignBinding
 import com.example.wavesoffood.databinding.ActivityStartBinding
 
-class LoginActivity : AppCompatActivity() {
-    private val binding:ActivityLoginBinding by lazy {
-        ActivityLoginBinding.inflate(layoutInflater)
+class SignActivity : AppCompatActivity() {
+    private val binding:  ActivitySignBinding by lazy{
+        ActivitySignBinding.inflate(layoutInflater)
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(binding.root)
-        binding.loginbutton.setOnClickListener {
-            val intent = Intent(this,SignActivity::class.java)
-            startActivity(intent)
-        }
-        binding.donthavebutton.setOnClickListener {
-            val intent = Intent(this,SignActivity::class.java)
+        binding.arhbutton.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
         }
 
--- app/src/main/java/com/example/wavesoffood/StartActivity.kt
+++ app/src/main/java/com/example/wavesoffood/StartActivity.kt
@@ -1,11 +1,20 @@
 package com.example.wavesoffood
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.wavesoffood.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+private val binding: ActivityStartBinding by lazy{
+    ActivityStartBinding.inflate(layoutInflater)
+}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)  // Asegúrate de que este layout existe
+        setContentView(binding.root)
+        binding.nextButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
