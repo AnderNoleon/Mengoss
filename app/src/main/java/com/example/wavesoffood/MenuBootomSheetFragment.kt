69fcc781607c9a606737b75a403e16bd4a1285cd
AnderNoleon <andersonomar428@gmail.com>
2024-10-12 11:55:48 -0600

    Boton de regresar

--- app/src/main/java/com/example/wavesoffood/MenuBootomSheetFragment.kt
+++ app/src/main/java/com/example/wavesoffood/MenuBootomSheetFragment.kt
@@ -1,10 +1,11 @@
 package com.example.wavesoffood
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.wavesoffood.adaptar.MenuAdaptear
 import com.example.wavesoffood.databinding.FragmentMenuBootomSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
@@ -23,7 +24,30 @@ class MenuBootomSheetFragment : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMenuBootomSheetBinding.inflate(inflater,container, false)
+
+        binding.buttonBack.setOnClickListener{
+            dismiss()
+        }
+
+
+        val menuFoodName = listOf("Burger","sandwich","mono","item","sandwich","mono")
+        val menuItemPrice = listOf("Q5","Q6","Q10","Q15","Q10","Q20")
+        val menuImage = listOf(
+            R.drawable.menu1 ,
+            R.drawable.menu2 ,
+            R.drawable.menu3 ,
+            R.drawable.menu4 ,
+            R.drawable.menu5 ,
+            R.drawable.menu6)
+        val adapter = MenuAdaptear(
+            ArrayList(menuFoodName),
+            ArrayList(menuItemPrice),
+            ArrayList(menuImage))
+        binding.menuRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.menuRecyclerView.adapter = adapter
         return binding.root
+
+
     }
 
     companion object {
