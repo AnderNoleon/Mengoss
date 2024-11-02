f7da2db6fb9bbe3233bc18fa0fcdf161312db197
AnderNoleon <andersonomar428@gmail.com>
2024-11-02 17:35:41 -0600

    Se hace el modulo de carrito a venta cliente

--- app/src/main/java/com/example/wavesoffood/Fragment/CartFragment.kt
+++ app/src/main/java/com/example/wavesoffood/Fragment/CartFragment.kt
@@ -1,11 +1,14 @@
 package com.example.wavesoffood.Fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.wavesoffood.CongrantsBottomSheet
+import com.example.wavesoffood.PayOutActivity
 import com.example.wavesoffood.R
 import com.example.wavesoffood.adaptar.CartAdapter
 import com.example.wavesoffood.databinding.CartItemBinding
@@ -39,6 +42,14 @@ class CartFragment : Fragment() {
         val adapter = CartAdapter(ArrayList(cartFoodName),ArrayList(cartItemPrice),ArrayList(cartImage))
         binding.cartRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.cartRecyclerView.adapter = adapter
+
+
+        binding.proceedButton.setOnClickListener {
+            val intent = Intent(requireContext(),PayOutActivity ::class.java)
+            startActivity(intent)
+        }
+
+
 
 
         return binding.root
